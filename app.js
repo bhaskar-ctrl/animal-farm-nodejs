cb4d06f6b428f41642619d6596796cb570259092
bhaskar-ctrl <hbhaskar.2510@gmail.com>
2025-11-01 13:06:22 -0400

    Refactor app.js with improved structure and fixes

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
 const express = require('express');
 const _ = require('underscore');
 
-var port = process.env.PORT || 8080;
-var animals = {
-    "cat": "meow",
-    "dog": "bark",
-    "eel": "hiss",
-    "bear": "growl",
-    "frog": "croak",
-    "lion": "roar"
-}
+const port = process.env.PORT || 8080;
+
+const animals = {
+  cat: "meow",
+  dog: "bark",
+  eel: "hiss",
+  bear: "growl",
+  frog: "croak",
+  lion: "roar"
+};
 
 function getAnimal() {
-  return animal = _.sample(Object.entries(animals));
+  return _.sample(Object.entries(animals));
 }
 
 const app = express();
 
-app.get('/', function(req, res){
-  const [animal_name, sound] = getAnimal();
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`George Orwell had a farm.<br />
-E-I-E-I-O<br />
-And on his farm he had a ${ animal_name }.<br />
-E-I-E-I-O<br />
-With a ${ sound }-${ sound } here.<br />
-And a ${ sound }-${ sound } there.<br />
-Here a ${ sound }, there a ${ sound }.<br />
-Everywhere a ${ sound }-${ sound }.<br />`);
-      res.end();
+app.get('/', (req, res) => {
+  const [animalName, sound] = getAnimal();
+  res.status(200).send(`
+    George Orwell had a farm.<br />
+    E-I-E-I-O<br />
+    And on his farm he had a ${animalName}.<br />
+    E-I-E-I-O<br />
+    With a ${sound}-${sound} here.<br />
+    And a ${sound}-${sound} there.<br />
+    Here a ${sound}, there a ${sound}.<br />
+    Everywhere a ${sound}-${sound}.<br />
+  `);
 });
 
-app.get('/api', function(req, res){
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify(animals));
-  res.end();
-})
-
-module.exports =  app.listen(port, () => {
-  console.log(`Launching server on http://localhost:${ port }`)
+app.get('/api', (req, res) => {
+  res.status(200).json(animals);
 });
+
+app.listen(port, () => {
+  console.log(`Launching server on http://localhost:${port}`);
+});
+
+module.exports = app;
